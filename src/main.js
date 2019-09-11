7fa17acdf20f06888f1a37b8ab4661cc1ed2f844
wanghani <504208173@qq.com>
2019-09-12 00:30:15 +0800

    项目第一天的简单实现

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/global.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+//导入字体图标
+import './assets/fonts/iconfont.css'
+
+
+
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
