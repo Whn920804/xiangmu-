a95e832c50e393dc6e609223685a93f50e07d462
wanghani <504208173@qq.com>
2019-09-15 08:45:32 +0800

    第二天的

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,13 @@
         <img src="../assets/logo.png" alt>
       </div>
       <!-- 登录表单区域 -->
-      <el-form :model="loginForm" :rules="rules" ref="loginFormRef" label-width="0px" class="login_form">
+      <el-form
+        :model="loginForm"
+        :rules="rules"
+        ref="loginFormRef"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
 
         <el-form-item prop="username">
@@ -23,7 +29,7 @@
         <!-- 按钮区域 -->
         <el-form-item class="btns">
           <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" plain   @click="resetLoginForm">重置</el-button>
+          <el-button type="info" plain @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -35,37 +41,45 @@ export default {
     return {
       //这是登录表单的数据绑定对象
       loginForm: {
-        username: "whn",
-        password: "123"
+        username: 'admin',
+        password: '123456'
       },
       rules: {
         username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 10个字符", trigger: "blur" }
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 10个字符', trigger: 'blur' }
         ],
         password: [
-          { required: true, message: "请输入登录密码", trigger: "blur" },
-          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
         ]
       }
-    };
+    }
   },
-  methods:{
-     resetLoginForm() {
+  methods: {
+    resetLoginForm() {
       // console.log(this);
       this.$refs.loginFormRef.resetFields()
     },
-    login(){
-        this.$refs.loginFormRef.validate(valid =>{
-            // console.log(valid);
-            //vaild 是一个布尔值
-            
-            
-        })
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(valid);
+        //vaild 是一个布尔值
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // console.log(data);
+        if (res.meta.status != 200) return this.$message.error('登录失败！')
+        this.$message.success('登录成功')
+        // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        console.log(res)
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
     }
   }
-
-};
+}
 </script>
 <style lang="less" scoped>
 .login_container {
